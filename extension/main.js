31e46adff144b9ca99b303e68b961c6cec00f94e
gKushagra <55290773+gKushagra@users.noreply.github.com>
2022-11-19 21:19:36 -0600

    initial attempt

--- extension/main.js
+++ extension/main.js
@@ -24,10 +24,47 @@
      *    the posting is created, the Editor should generate alpha resume which should be available
      *    to the user in the extension popup.
      */
-    
-    chrome.tabs.query({ active: true, lastFocusedWindow: true }, tabs => {
-        let url = tabs[0].url;
-        document.getElementById('current-url').href = url;
+    const getUrlButton = document.getElementById('getUrl');
+    // const getViewsButton = document.getElementById('getViews');
+    // const getBackgroundPageButton = document.getElementById('getBackgroundPage');
+
+    getUrlButton.addEventListener('click', () => {
+        chrome.tabs.query({ active: true, lastFocusedWindow: true }, tabs => {
+            // console.log(tabs);
+            // chrome.tabs.get(tabs[0].id)
+            //     .then(data => console.log(data))
+            //     .catch(error => console.error(error));
+            let url = tabs[0].url;
+            // const linkEl = document.createElement('a');
+            // linkEl.href = url;
+            // linkEl.target = '_blank';
+            // linkEl.click();
+            console.log(url);
+            fetch(
+                "http://localhost:5454/html?url=" + url,
+                { method: "get", mode: "cors" }
+            )
+                .then(function (response) {
+                    // The API call was successful!
+                    return response.text();
+                }).then(function (html) {
+                    // This is the HTML from our response as a text string
+                    console.log(html);
+                }).catch(function (err) {
+                    // There was an error
+                    console.warn('Something went wrong.', err);
+                });
+        });
     });
+
+    // getViewsButton.addEventListener('click', () => {
+    //     const views = chrome.extension.getViews();
+    //     console.log(views);
+    // });
+
+    // getBackgroundPageButton.addEventListener('click', () => {
+    //     const backgroundPage = chrome.extension.getBackgroundPage();
+    //     console.log(backgroundPage);
+    // });
 
 })();
