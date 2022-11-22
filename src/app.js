aad3fcf9865e01d866453ec68c4c02da06433203
gKushagra <55290773+gKushagra@users.noreply.github.com>
2022-11-21 22:54:16 -0600

    refactor

--- src/app.js
+++ src/app.js
@@ -9,6 +9,7 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cors());
+app.use(express.static('www'))
 const knex = require('knex')({
     client: 'sqlite3',
     connection: {
